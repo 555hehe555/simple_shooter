8fa72d6ec196d48f77c2f78a74345a18e673d2f2
555hehe555 <123888406+555hehe555@users.noreply.github.com>
2024-03-19 18:56:28 +0200

    show hero and zombie

---
+++ classes.py
@@ -0,0 +1,36 @@
+import pygame
+
+pygame.init()
+
+bullets = pygame.sprite.Group()
+
+
+class GameSprite(pygame.sprite.Sprite):
+    def __init__(self, img, window, x=0, y=0, width=10, height=10, health=100):
+        pygame.sprite.Sprite.__init__(self)
+        self.img = pygame.transform.scale(pygame.image.load(img), (width, height))
+        self.rect = self.img.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.width = width
+        self.height = height
+        self.health = health
+        self.window = window
+
+    def create(self):
+        self.window.blit(self.img, (self.rect.x, self.rect.y))
+
+
+class Bullet(GameSprite):
+    ...
+
+
+class Hero(GameSprite):
+    def fire(self):
+        bullet = Bullet("./img/window_icon128", self.rect.centerx, self.rect.centery, 20, 10, 1)
+        bullets.add(bullet)
+
+
+class Zombie(GameSprite):
+    def run(self):
+        self.rect.x += 10
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from classes import *
 import sys
 import pygame
 import time
@@ -6,33 +7,38 @@ import random
 pygame.init()
 
 window_icon = pygame.image.load("./img/window_icon512.png")
+hero_img = "./img/window_icon128.png"
+zombie_img = "./img/windows_icon64.png"
 
-SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 1024
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
 bg_color = (115, 81, 132)
 pygame.display.set_caption("Simple shooter!!!")
 pygame.display.set_icon(window_icon)
 window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-# window.fill(bg_color)
+window.fill(bg_color)
+
+hero = GameSprite(hero_img, window, 400, 300, 100, 100)
 
 is_running = True
-pygame.display.update()
+
+zombies = list()
+
+for i in range(random.randint(5, 10)):
+    zombie = Zombie(zombie_img, window, random.randint(0, 800), 300, 50, 70)
+    print(zombie, zombies)
+    zombies.append(zombie)
+    print(zombie, zombies)
+
 while is_running:
+    # window.blit(window_icon,(0,0))
+    hero.create()
 
+    for item in zombies:
+        item.create()
 
-
-    time.sleep(0.05)
-    bg_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-    window.fill(bg_color)
     pygame.display.update()
-    time.sleep(0.05)
-    bg_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-    window.fill(bg_color)
-    pygame.display.flip()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
             pygame.quit()
-            
-pygame.quit()
-sys.exit()
