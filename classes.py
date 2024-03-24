655b7a178ad71372342c0ecc999b7085b44852f7
555hehe555 <123888406+555hehe555@users.noreply.github.com>
2024-03-24 21:32:55 +0200

    zombies attack the player

--- classes.py
+++ classes.py
@@ -4,11 +4,13 @@ pygame.init()
 
 bullets = pygame.sprite.Group()
 
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+
 
 class GameSprite(pygame.sprite.Sprite):
-    def __init__(self, img, window, x=0, y=0, width=10, height=10, health=100):
+    def __init__(self, img, window, x=0, y=0, width=10, height=10, health=100, speed=10):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.transform.scale(pygame.image.load(img), (width, height))
+        self.image = pygame.transform.scale(pygame.image.load(img).convert_alpha(), (width, height))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -16,9 +18,17 @@ class GameSprite(pygame.sprite.Sprite):
         self.height = height
         self.health = health
         self.window = window
+        self.speed = speed
 
-    def create(self):
+    def reset(self):
         self.window.blit(self.image, (self.rect.x, self.rect.y))
+
+    #
+    # def collidepoint(self, x, y):
+    #     return self.rect.collidepoint(x, y)
+
+    def colliderect(self, rect):
+        return self.rect.colliderect(rect)
 
 
 class Bullet(GameSprite):
@@ -26,11 +36,35 @@ class Bullet(GameSprite):
 
 
 class Hero(GameSprite):
+    def show_coords(self):
+        return [self.rect.x, self.rect.y]
+
     def fire(self):
         bullet = Bullet("./img/window_icon128", self.rect.centerx, self.rect.centery, 20, 10, 1)
         bullets.add(bullet)
 
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and self.rect.x > 0:
+            self.rect.x -= self.speed
+        if keys[pygame.K_RIGHT] and self.rect.x < SCREEN_WIDTH - self.width:
+            self.rect.x += self.speed
+
+        if keys[pygame.K_UP] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[pygame.K_DOWN] and self.rect.y < SCREEN_HEIGHT - self.height:
+            self.rect.y += self.speed
+
 
 class Zombie(GameSprite):
-    def run(self):
-        self.rect.x += 10
+    def update(self, hero_coord):
+        if self.rect.x != hero_coord[0] or self.rect.y != hero_coord[1]:
+            if self.rect.x > hero_coord[0]:
+                self.rect.x -= 1
+            elif self.rect.x < hero_coord[0]:
+                self.rect.x += 1
+
+            if self.rect.y > hero_coord[1]:
+                self.rect.y -= 1
+            elif self.rect.y < hero_coord[1]:
+                self.rect.y += 1
--- main.py
+++ main.py
@@ -7,8 +7,6 @@ import random
 pygame.init()
 
 window_icon = pygame.image.load("./img/window_icon512.png")
-hero_img = "./img/window_icon128.png"
-zombie_img = "./img/windows_icon64.png"
 
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
 bg_color = (115, 81, 132)
@@ -17,25 +15,42 @@ pygame.display.set_icon(window_icon)
 window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 window.fill(bg_color)
 
-hero = GameSprite(hero_img, window, 400, 300, 100, 100)
+bg_img = pygame.transform.scale(pygame.image.load("./img/img.png").convert(), (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+hero_img = "./img/window_icon128.png"
+zombie_img = "./img/windows_icon64.png"
+
+hero = Hero(hero_img, window, (SCREEN_WIDTH/2)-40, (SCREEN_HEIGHT/2)-50, 100, 100)
 
 zombies = pygame.sprite.Group()
 
-for i in range(1, 6):
-    zombie = Zombie("./img/windows_icon64.png", window, 300, 400, 100, 100)
+for i in range(1000):
+    zombie = Zombie("./img/img_1.png", window, random.randint(0, SCREEN_WIDTH-100),
+                    random.randint(0, SCREEN_HEIGHT-100), 100, 100)
     zombies.add(zombie)
 
 is_running = True
-
+clock = pygame.time.Clock()
 while is_running:
-    # window.blit(window_icon,(0,0))
-    hero.create()
+    print(clock.get_fps())
+    window.blit(bg_img, (0, 0))
+    hero.reset()
+    hero.update()
 
-    zombies.update()
+    zombies.update(hero.show_coords())
+
     zombies.draw(window)
+
+    for z in zombies:
+        a = z.colliderect(hero.rect)
+        if a:
+            print("dfggg")
+
     pygame.display.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
             pygame.quit()
+
+    clock.tick(60)
